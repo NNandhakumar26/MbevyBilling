63fec4a4e9a39cf8bf34ff54fa85c6219771dfd3
Nandha Kumar <57140186+NNandhakumar26@users.noreply.github.com>
2021-11-29 22:58:57 +0530

    Removed the parent widget error

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -11,9 +11,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "ca83b04474759e4f5f0c07dc87fd08d8",
-"/": "ca83b04474759e4f5f0c07dc87fd08d8",
-"main.dart.js": "9d8171dee16cb2da62ea1a3ec4338db1",
+"index.html": "b0e9aec038c1c32d32ff1ca00c4c901d",
+"/": "b0e9aec038c1c32d32ff1ca00c4c901d",
+"main.dart.js": "765f2b537239f28bd3ddcbd6ab2ff0c1",
 "manifest.json": "5ea8741259058534cc82604f8b55cd1b",
 "version.json": "957082efed61701b688bf144d05a3914"
 };
